10c54bb4ce3ea933d6ae09dbc132b186dfc39f1f
Didjok <madiar_21@inbox.ru>
2024-03-13 14:27:01 +0500

    create new links: create.html and success.html, and add forms.py

--- post/urls.py
+++ post/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.posts, name='posts'),
+    path('create/', views.create_view, name='create_post'),
     # Другие маршруты вашего модуля...
 ]
--- post/views.py
+++ post/views.py
@@ -1,8 +1,19 @@
 from django.http import HttpResponse
 from .models import Post
 from django.shortcuts import render
+from .forms import PostForm
 
 def posts(request):
     posts = Post.objects.all()
     context = {'posts': posts}
     return render(request, "post/index.html", context)
+
+def create_view(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()  # Сохраняем данные в базу данных
+            return render(request, 'post/success.html')
+    else:
+        form = PostForm()
+    return render(request, 'post/create.html', {'form': form})
